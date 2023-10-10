e4c5dec414fb15530e3694ed2886d3e7039b6fb4
chb <gyqls234@gmail.com>
2023-10-11 00:49:55 +0900

    -

--- graph/Main_Babyshark.java
+++ graph/Main_Babyshark.java
@@ -9,8 +9,8 @@ import java.util.StringTokenizer;
 
 public class Main_Babyshark {
     public static void main(String[] args) throws IOException {
-        int[] dR = {-1, 0, 1, 0};
-        int[] dC = {0, -1, 0, 1};
+        int[] dR = {-1, 0, 0, 1};
+        int[] dC = {0, -1, 1, 0};
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] map = new int[N][N];
@@ -23,7 +23,7 @@ public class Main_Babyshark {
             for (int j = 0; j < N; j++) {
                map[i][j] = Integer.parseInt(st.nextToken());
                if(map[i][j] == 9){
-                   start = new int[]{i, j, 1};
+                   start = new int[]{i, j, 0};
                    map[i][j] = 0;
                }
             }
@@ -46,16 +46,25 @@ public class Main_Babyshark {
                 if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
                 if(map[nr][nc] > size) continue;
                 visited[nr][nc] = true;
+
                 if(map[nr][nc] != 0 && map[nr][nc] < size ){
+                    System.out.println(nr);
+                    System.out.println(nc);
+                    System.out.println(map[nr][nc]);
                     if(++consumed == size) {
                         size++;
                         consumed = 0;
                     }
                     map[nr][nc] = 0;
-                    elapsed += curr[2];
+                    elapsed += curr[2]+1;
+                    System.out.println(elapsed);
+
+                    System.out.println("=========");
+
                     queue = new ArrayDeque<>();
                     visited = new boolean[N][N];
-                    queue.offer(new int[]{nr, nc, 1});
+                    queue.offer(new int[]{nr, nc, 0});
+                    break; /* had to sout all curr[2] to figure this one */
                 }else{
                     queue.offer(new int[]{nr, nc, curr[2]+1});
                 }
@@ -83,5 +92,13 @@ public class Main_Babyshark {
 0 0 0 0 0 6
 0 0 0 0 0 9
         * */
+        /*
+        https://www.acmicpc.net/board/view/30275
+        * 같은 최단거리에 있는 먹이들 중 우선순위는 위에 있는 물고기, 높이도 같다면 왼쪽에 있는 물고기를 섭취해야 하는데 단순히 BFS만 사용하면 4번 테스트 케이스에서 오류가 난다.
+        만약 우선순위를 제대로 고려하지 않고 그냥 BFS를 사용했다면 이 테스트 케이스에서 56이 나올 것이다.
+
+
+
+이유는 (0,2)에서 길이 3짜리 물고기를 먹은 뒤 (0,4) 혹은 (1,1) 둘 모두 최단거리 2 에 있는 물고기고, 먹을 수 있다. 문제 조건에 따르면 (0,4)에 있는 물고기가 더 우선순위가 높다. 그러나 단순 BFS로 위,왼쪽,오른쪽,아래 순서로의 탐색만 고려했을 경우 (1,1)에 있는 물고기를 먹게 된다.*/
     }
 }
