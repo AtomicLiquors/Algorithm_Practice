733a95e8f575f0b32fde26f4e96e283a50d734ba
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2023-12-29 11:31:16 +0900

    :pencil2: 2023-12-29

---
+++ hyobin/javascript/binary/programmers_120885.js
@@ -0,0 +1,7 @@
+// 이진수 더하기
+function solution(bin1, bin2) {
+    return ( parseInt(bin1, 2) + parseInt(bin2, 2) ).toString(2) ;
+}
+
+// 특정 진수를 나타내는 String을 parseInt를 이용해 알맞은 진수로 변환하기.
+// toString()는 10진수를 특정 진수로 변환해 준다.
