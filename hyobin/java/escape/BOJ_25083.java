b7592cac443805ee727c2a9bdb80281b24c21cf2
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-01-09 17:24:02 +0900

    Create BOJ_25083.java

---
+++ hyobin/java/escape/BOJ_25083.java
@@ -0,0 +1,15 @@
+// 새싹: 클래스 1 완료
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r\'\"7").append('\n');
+        sb.append("r`-_   ,\'  ,/").append('\n');
+        sb.append(" \\. \". L_r\'").append('\n');
+        sb.append("   `~\\/").append('\n');
+        sb.append("      |").append('\n');
+        sb.append("      |").append('\n');
+        System.out.println(sb);
+    }
+}
