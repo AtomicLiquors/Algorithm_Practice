152b528f0287041134050c049731fcd4b5c23e2e
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-01-20 17:57:42 +0900

    Create BOJ_2439.java

---
+++ hyobin/java/BOJ_2439.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
