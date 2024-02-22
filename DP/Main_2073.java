c4f8b05e46095d729a53776cbf6573cc5630ecfc
chb <gyqls234@gmail.com>
2024-02-22 11:36:51 +0900

    solving BOJ 20057

--- DP/Main_2073.java
+++ DP/Main_2073.java
@@ -3,12 +3,9 @@ package DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_2073 {
-    static int[][] DP;
-    static int[] capacities;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,30 +15,25 @@ public class Main_2073 {
         int D = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
 
-        int[] lengths = new int[P+1];
-        capacities = new int[P+1];
+        int[] lengths = new int[P];
+        int[] capacities = new int[P];
 
-        for (int i = 1; i <= P; i++) {
+        for (int i = 0; i < P; i++) {
             st = new StringTokenizer(br.readLine());
             lengths[i] = Integer.parseInt(st.nextToken());
             capacities[i] = Integer.parseInt(st.nextToken());
         }
 
-        DP = new int[P+1][D+1];
+        int[] DP = new int[D+1];
+        DP[0] = Integer.MAX_VALUE;
 
-        for (int i = 1; i <= P; i++) {
-            int prev = i-1;
-            DP[i][0] = Integer.MAX_VALUE;
-
-            for (int j = 1; j <= D; j++) {
-                int rem = j - lengths[i];
-                if(rem >= 0 && DP[prev][rem] > 0)
-                    DP[i][j] = Math.max(DP[prev][j], Math.min(capacities[i], DP[i][rem]));
+        for (int i = 0; i < P; i++) {
+            for (int j = D; j >= lengths[i]; j--) {
+                DP[j] = Math.max(DP[j], Math.min(DP[j-lengths[i]], capacities[i]));
             }
         }
 
-        System.out.println(Arrays.deepToString(DP));
-        System.out.println(DP[P][D]);
+        System.out.println(DP[D]);
     }
 
 }
