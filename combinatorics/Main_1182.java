f450aa5fea7fabd5da481a92ca72573a5b57ada1
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-05-10 21:30:58 +0900

    BOJ 1182 SOLVED

--- combinatorics/Main_1182.java
+++ combinatorics/Main_1182.java
@@ -3,9 +3,51 @@ package combinatorics;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_1182 {
+
+    static boolean[] isSelected;
+    static int N, S, answer, array[];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        answer = 0;
+        array = new int[N];
+        isSelected = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        subset(0, 0);
+
+        System.out.println(answer);
+    }
+
+    static void subset(int idx, int cnt){
+        if(cnt == N){
+            int sum = 0;
+            boolean flag = false;
+            for (int i = 0; i < N; i++) {
+                if(isSelected[i]){
+                    sum += array[i];
+                    flag = true;
+                }
+            }
+            if(sum == S && flag) answer++;
+            return;
+        }
+
+        isSelected[idx] = true;
+        subset(idx+1, cnt+1);
+        isSelected[idx] = false;
+        subset(idx+1, cnt+1);
     }
 }
