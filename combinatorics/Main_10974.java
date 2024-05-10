eebbf0c54fe9eb03247f39a0d8d923bcef085a65
chb <gyqls234@gmail.com>
2024-05-10 21:28:36 +0900

    10974 SOL

---
+++ combinatorics/Main_10974.java
@@ -0,0 +1,45 @@
+package combinatorics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10974 {
+
+    static int N;
+    static boolean[] selected;
+    static int[] numbers;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        selected = new boolean[N+1];
+        numbers = new int[N];
+
+        perm(0);
+
+        System.out.print(sb);
+    }
+
+    static void perm(int cnt){
+        if(cnt == N){
+            for (int i = 0; i < N; i++) {
+               sb.append(numbers[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if(selected[i])
+                continue;
+
+            numbers[cnt] = i;
+            selected[i] = true;
+            perm(cnt+1);
+            selected[i] = false;
+        }
+    }
+}
