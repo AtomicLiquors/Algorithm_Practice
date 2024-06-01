30827f5dec0e1e1afb3aafaecf13761beab1f0b9
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-06-01 11:22:25 +0900

    Update programmers_77484.js

--- hyobin/javascript/map/programmers_77484.js
+++ hyobin/javascript/map/programmers_77484.js
@@ -37,3 +37,7 @@ function solution(lottos, win_nums) {
 
     return [rank[maxCount], rank[minCount]];
 }
+
+// !0이 true 임을 이용했군요 
+// 매번 includes를 실행하면 시간복잡도 측면에서 최악이네요. 만약 6개가 아니라 100개, 1000개를 선택하는 문제였으면...
+// filter 두개는 for문 하나로 합칠 수 있을 것 같습니다.
