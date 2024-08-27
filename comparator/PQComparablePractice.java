045bb6393d7c33585aaeddf51a99444684cc929a
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-08-28 06:18:39 +0900

    directory name changed

--- comparator/PQComparablePractice.java
+++ comparator/PQComparablePractice.java
@@ -1,6 +1,5 @@
-package priorityqueue;
+package comparator;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 
--- comparator/PQComparatorPractice.java
+++ comparator/PQComparatorPractice.java
@@ -1,4 +1,4 @@
-package priorityqueue;
+package comparator;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
