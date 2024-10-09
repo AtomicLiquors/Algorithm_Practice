e56ab10fc3082669fed2e491fc531a7532dc3ac8
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-10-09 19:35:27 +0900

    Floyd

---
+++ DP/Floyd/Main_11404.java
@@ -0,0 +1,57 @@
+package DP.Floyd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11404 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int MAX = 100_001 * 99;
+        // 최댓값 설정 : 최대 비용은 편도 버스 거리가 아니라 편도 버스 거리(100000+) * 최대 정점 갯수(99)여야 한다.
+
+        int[][] DP = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(DP[i], MAX);
+        }
+
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            DP[start][end] = Math.min(cost, DP[start][end]);
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                if (i == k)
+                    continue;
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || j == k)
+                        continue;
+
+                    DP[i][j] = Math.min(DP[i][j], DP[i][k] + DP[k][j]);
+
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sb.append(DP[i][j] == MAX ? 0 : DP[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
---
+++ Mathematics/Main_2914.java
@@ -0,0 +1,18 @@
+package Mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2914 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * (I-1) + 1);
+    }
+}
