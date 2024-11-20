2a237c652421085e9ef51d42c4c7eaa8705a79b2
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-11-20 18:38:52 +0900

    standard subset updated

---
+++ combinatorics/subset/SubsetStandard.java
@@ -0,0 +1,27 @@
+package combinatorics.subset;
+
+public class SubsetStandard {
+    static int N = 5;
+
+    static boolean isSelected[] = new boolean[N+1];
+
+    public static void main(String[] args) {
+        subset(1);
+    }
+
+
+    static void subset(int cnt){
+        if(cnt == N+1) {
+            for (int i = 1; i <= N; i++) {
+                if(isSelected[i]) System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        isSelected[cnt] = true;
+        subset(cnt+1);
+        isSelected[cnt] = false;
+        subset(cnt+1);
+    }
+}
