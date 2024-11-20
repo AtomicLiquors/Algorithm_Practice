52fe532211770364b1876b4095bfbf2eea9c397e
chb <gyqls234@gmail.com>
2024-11-20 23:05:51 +0900

    BOJ 15686 SOLVED

--- combinatorics/subset/Main_15686.java
+++ combinatorics/subset/Main_15686.java
@@ -8,16 +8,11 @@ import java.util.*;
 public class Main_15686 {
 
     static int N, M, houseLength, storeLength, map[][], answer = Integer.MAX_VALUE;
-    static int[] dR = {0, 0, -1, 1};
-    static int[] dC = {-1, 1, 0, 0};
+
     static boolean[] isSelected;
     static List<int[]> storeLocations, houseLocations;
-    static Queue<int[]> marked = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
-        // 틀린 답이 뜨는 이유를 찾아라.
-        // r, c가 가게를 찾지 못하면 distance가 0이 될 수 있다.
-        // 가게를 똑바로 복구하지 않았다면 가게를 찾지 못했을 수 있다.
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -53,13 +48,28 @@ public class Main_15686 {
         if(idx == storeLength){
             if(cnt == 0)
                 return;
+            int currentCityChickenDist = 0;
+            for (int hIdx = 0; hIdx < houseLength; hIdx++) {
+                int minDist = Integer.MAX_VALUE;
+                int currentDist;
+                for (int sIdx = 0; sIdx < storeLength; sIdx++) {
+                    if(isSelected[sIdx]){
+                        int[] house = houseLocations.get(hIdx);
+                        int houseR = house[0];
+                        int houseC = house[1];
 
-            for (int i = 0; i < storeLength; i++) {
-                if(!isSelected[i])
-                    removeStore(i);
+                        int[] store = storeLocations.get(sIdx);
+                        int storeR = store[0];
+                        int storeC = store[1];
+
+                        currentDist = Math.abs(houseR - storeR) + Math.abs(houseC - storeC);
+                        minDist = Math.min(currentDist, minDist);
+                    }
+                }
+                currentCityChickenDist += minDist;
+
             }
-            findMinimumDistance();
-            restoreStores();
+            answer = Math.min(currentCityChickenDist, answer);
             return;
         }
 
@@ -71,73 +81,4 @@ public class Main_15686 {
         subset(cnt, idx+1);
     }
 
-    static void removeStore(int i){
-        int[] loc = storeLocations.get(i);
-        int r = loc[0];
-        int c = loc[1];
-        map[r][c] = 0;
-        marked.offer(new int[]{r, c});
-    }
-
-    static void restoreStores(){
-        while(!marked.isEmpty()){
-            int[] loc = marked.poll();
-            int r = loc[0];
-            int c = loc[1];
-            map[r][c] = 2;
-        }
-    }
-
-    static void findMinimumDistance(){
-        int regionalChickenDistance = 0;
-
-        for (int i = 0; i < houseLength; i++)
-            regionalChickenDistance += getChickenDistance(i);
-
-        answer = Math.min(answer, regionalChickenDistance);
-    }
-
-    static int getChickenDistance(int i){
-        int[] house = houseLocations.get(i);
-        int startR = house[0];
-        int startC = house[1];
-
-        int distance = Integer.MAX_VALUE;
-
-        boolean[][] visited = new boolean[N][N];
-        Queue<int[]> que = new ArrayDeque<>();
-        que.offer(new int[]{startR, startC, 0});
-        visited[startR][startC] = true;
-
-        while(!que.isEmpty()){
-            int[] loc = que.poll();
-            int r = loc[0];
-            int c = loc[1];
-            int offset = loc[2];
-
-            if(map[r][c] == 2){
-                distance = offset;
-                break;
-            }
-
-
-            int nr, nc;
-            for (int dir = 0; dir < 4; dir++) {
-                nr = r + dR[dir];
-                nc = c + dC[dir];
-
-                if(isOuttaBound(nr, nc) || visited[nr][nc])
-                    continue;
-
-                visited[nr][nc] = true;
-                que.offer(new int[]{nr, nc, offset+1});
-            }
-        }
-
-        return distance;
-    }
-
-    static boolean isOuttaBound(int r, int c){
-        return r < 0 || c < 0 || r >= N || c >= N;
-    }
 }
