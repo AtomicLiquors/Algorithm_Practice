6f7cd7d4e7016ff1b598a71f71da406697988795
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2024-11-21 12:53:37 +0900

    :memo: Create CombinationStandard.java

---
+++ combinatorics/combination/examples/CombinationStandard.java
@@ -0,0 +1,23 @@
+public class Main{
+
+    static int N = 4;
+    static int R = 3;
+
+    static int input[] = new int[N];
+    static int numbers[] = new int [R];
+
+    public static void main(String[] args) {
+        comb(0, 1);
+    }
+
+    static void comb(int cnt, int start){
+        if(cnt == R){
+            System.out.println(Arrays.toString(numbers));
+        }else{
+            for (int i = start; i <= N; i++) {
+                numbers[cnt] = i;
+                comb(cnt+1, i+1);
+            }
+        }
+    }
+}
