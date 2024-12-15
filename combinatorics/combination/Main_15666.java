ea32fddfd1f2609fc4931484782c8592e37628bd
chb <gyqls234@gmail.com>
2024-12-15 13:38:29 +0900

    :rocket: BOJ 15666 solved

--- combinatorics/combination/Main_15666.java
+++ combinatorics/combination/Main_15666.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main_15666 {
 
-    static int N, M, arr[];
-    static StringBuilder sb = new StringBuilder();
+    static int N, M, arr[], selected[];
+    static StringBuilder keySB = new StringBuilder();
+    static StringBuilder answerSB = new StringBuilder();
+    static Set<String> set = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,6 +23,7 @@ public class Main_15666 {
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[N];
+        selected = new int[M];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
@@ -28,24 +33,27 @@ public class Main_15666 {
 
         comb(0,0);
 
+        System.out.print(answerSB);
 
-        System.out.print(sb);
         br.close();
     }
 
     static void comb(int start, int cnt){
         if(cnt == M){
-            sb.append('\n');
+            keySB = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                keySB.append(selected[i]).append(' ');
+            }
+
+            String key = keySB.toString();
+            if(set.add(key))
+                answerSB.append(key).append('\n');
             return;
         }
 
         for (int i = start; i < N; i++) {
-            sb.append(arr[i]).append(' ');
+            selected[cnt] = arr[i];
             comb(i, cnt+1) ;
         }
-    }
-
-    static void reversalComb(){
-
     }
 }
