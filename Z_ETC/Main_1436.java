f1b95d25a733c4235ad985242856feeec7a03f5b
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2024-12-22 11:31:28 +0900

    SOLVED BOJ 1436

---
+++ Z_ETC/Main_1436.java
@@ -0,0 +1,24 @@
+package Z_ETC;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean notFound = true;
+
+        int title = 666;
+        int cnt = 0;
+        while(notFound){
+            if((title + "").contains("666") && ++cnt == N)
+                break;
+            title++;
+        }
+
+        System.out.println(title);
+    }
+}
