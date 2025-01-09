7656016f918fdf0ccd65236af55c756240511e60
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-01-09 18:43:31 +0900

    :white_check_mark: solved HSAT_7628

---
+++ HSAT_7628.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HSAT_7628 {
+
+    static int N, heaters[], answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        heaters = new int[N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            heaters[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, heaters[i]);
+        }
+
+        for (int i = 2; i <= max; i++) {
+            int cnt = 0;
+            for (int heater : heaters) {
+                if(heater % i == 0)
+                    cnt++;
+            }
+            answer = Math.max(cnt, answer);
+        }
+
+        System.out.println(answer);
+    }
+}
